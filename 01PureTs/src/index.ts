6bf1ba36b850e32b9f6734148e708b7c08f07ab5
Akash Shridharan <akashshridharan2802@gmail.com>
2023-08-15 10:26:53 +0530

    private public

--- 01PureTs/src/index.ts
+++ 01PureTs/src/index.ts
@@ -1,13 +1,30 @@
+// class User {
+//     public email: string  // it should be defined before using in constructor in TS, but in JS its not needed
+//     // everything we dont mark is by default public and can be accessed and changed globally
+//     private name: string
+//     readonly city: string = "abad" // it compulsorily needs initialization
+//     private city2: string = "dubai"
+//     #city3: string = "patna" // another way to declare private, but mainly it is used in JS
+//     constructor(email: string, name: string) {
+//         this.email = email;
+//         this.name = name;
+//     }
+// }
+
+// professional way to declare above class
 class User {
-    email: string  // it should be defined before using in constructor in TS, but in JS its not needed
-    name: string
+    
     readonly city: string = "abad" // it compulsorily needs initialization
-    constructor(email: string, name: string) {
+    
+    constructor(public email: string, private name: string) {
         this.email = email;
         this.name = name;
     }
-}
+} 
 
 // const akash = new User(email: "ak@com", name: "akash")  // wrong way
 const akash = new User( "ak@com",  "akash")  // correct way
-// akash.city = "delhi";
+akash.city // you can access it but not change it in readonly
+// akash.city2 // in private u cant change and access
+// akash.city3 
+
