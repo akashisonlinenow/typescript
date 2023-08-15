c4e2d60bcffd1bd8b4faba7f50417ec6449cef7d
Akash Shridharan <akashshridharan2802@gmail.com>
2023-08-15 10:40:45 +0530

    get getters and setters

--- 01PureTs/dist/index.js
+++ 01PureTs/dist/index.js
@@ -1,4 +1,5 @@
 "use strict";
+// ----------------------private and public ------------------------------------------------------------------------------------------------------
 // class User {
 //     public email: string  // it should be defined before using in constructor in TS, but in JS its not needed
 //     // everything we dont mark is by default public and can be accessed and changed globally
@@ -12,17 +13,45 @@
 //     }
 // }
 // professional way to declare above class
+// class User {
+//     readonly city: string = "abad" // it compulsorily needs initialization
+//     constructor(public email: string, private name: string) {
+//         this.email = email;
+//         this.name = name;
+//     }
+// } 
+// // const akash = new User(email: "ak@com", name: "akash")  // wrong way
+// const akash = new User( "ak@com",  "akash")  // correct way
+// akash.city // you can access it but not change it in readonly
+// // akash.city2 // in private u cant change and access
+// // akash.city3 
+// --------------------------------------- getters and setters -------------------------------------------------------------------------------
 class User {
     constructor(email, name) {
         this.email = email;
         this.name = name;
+        this._courseCount = 1;
         this.city = "abad"; // it compulsorily needs initialization
         this.email = email;
         this.name = name;
+    }
+    deleteToken() {
+        console.log("Token deleted");
+    }
+    get getAppleEmail() {
+        return `apple${this.email}`;
+    }
+    get courseCount() {
+        return this._courseCount;
+    }
+    set courseCount(courseNum) {
+        if (courseNum <= 1) {
+            throw new Error("Course count should be more than 1");
+        }
+        this._courseCount = courseNum;
     }
 }
 // const akash = new User(email: "ak@com", name: "akash")  // wrong way
 const akash = new User("ak@com", "akash"); // correct way
 akash.city; // you can access it but not change it in readonly
-// akash.city2 // in private u cant change and access
-// akash.city3 
+// akash.deleteToken()
--- 01PureTs/src/index.ts
+++ 01PureTs/src/index.ts
@@ -1,3 +1,4 @@
+// ----------------------private and public ------------------------------------------------------------------------------------------------------
 // class User {
 //     public email: string  // it should be defined before using in constructor in TS, but in JS its not needed
 //     // everything we dont mark is by default public and can be accessed and changed globally
@@ -12,7 +13,26 @@
 // }
 
 // professional way to declare above class
+// class User {
+    
+//     readonly city: string = "abad" // it compulsorily needs initialization
+    
+//     constructor(public email: string, private name: string) {
+//         this.email = email;
+//         this.name = name;
+//     }
+// } 
+
+// // const akash = new User(email: "ak@com", name: "akash")  // wrong way
+// const akash = new User( "ak@com",  "akash")  // correct way
+// akash.city // you can access it but not change it in readonly
+// // akash.city2 // in private u cant change and access
+// // akash.city3 
+
+// --------------------------------------- getters and setters -------------------------------------------------------------------------------
 class User {
+
+    private _courseCount = 1
     
     readonly city: string = "abad" // it compulsorily needs initialization
     
@@ -20,11 +40,28 @@ class User {
         this.email = email;
         this.name = name;
     }
+
+    private deleteToken() {
+        console.log("Token deleted")
+    }
+
+    get getAppleEmail(): string{
+        return `apple${this.email}`
+    }
+
+    get courseCount(): number {  // private ke saath chedkhani krni hai toh use getters and setters
+        return this._courseCount
+    }
+    set courseCount(courseNum) {  // setter method does not return anything
+        if(courseNum<=1) {
+            throw new Error("Course count should be more than 1")
+        }
+        this._courseCount = courseNum
+    }
+
 } 
 
 // const akash = new User(email: "ak@com", name: "akash")  // wrong way
 const akash = new User( "ak@com",  "akash")  // correct way
 akash.city // you can access it but not change it in readonly
-// akash.city2 // in private u cant change and access
-// akash.city3 
-
+// akash.deleteToken()
