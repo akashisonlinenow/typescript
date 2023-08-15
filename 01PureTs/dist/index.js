1b87812a82214574758bcdea643e6823298367ef
Akash Shridharan <akashshridharan2802@gmail.com>
2023-08-15 10:15:41 +0530

    classes

--- 01PureTs/dist/index.js
+++ 01PureTs/dist/index.js
@@ -1,3 +1,11 @@
 "use strict";
-console.log("typescript is here");
-console.log("typescript is amazing");
+class User {
+    constructor(email, name) {
+        this.city = "abad"; // it compulsorily needs initialization
+        this.email = email;
+        this.name = name;
+    }
+}
+// const akash = new User(email: "ak@com", name: "akash")  // wrong way
+const akash = new User("ak@com", "akash"); // correct way
+// akash.city = "delhi";
--- 01PureTs/src/index.ts
+++ 01PureTs/src/index.ts
@@ -1,3 +1,13 @@
-console.log("typescript is here")
+class User {
+    email: string  // it should be defined before using in constructor in TS, but in JS its not needed
+    name: string
+    readonly city: string = "abad" // it compulsorily needs initialization
+    constructor(email: string, name: string) {
+        this.email = email;
+        this.name = name;
+    }
+}
 
-console.log("typescript is amazing")
+// const akash = new User(email: "ak@com", name: "akash")  // wrong way
+const akash = new User( "ak@com",  "akash")  // correct way
+// akash.city = "delhi";
