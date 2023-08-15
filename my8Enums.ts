5cd55130450a576c981b9d5e5f90e80374d8b911
Akash Shridharan <akashshridharan2802@gmail.com>
2023-08-15 08:57:53 +0530

    enums

--- my8Enums.ts
+++ my8Enums.ts
@@ -2,8 +2,8 @@
 const enum SeatChoice {
     AISLE = "aisle", // AISLE =0 (by default)
     MIDDLE = 22,    // default, MIDDLE=1
-    WINDOW,   // default, WINDOW=1
-    FOURTH    // default, FOURTH=1
+    WINDOW,   // default, WINDOW=2
+    FOURTH    // default, FOURTH=3
 }
 
 const akSeat = SeatChoice.AISLE
